bdd5cb32590a98dd99270c87442f8687ebe462ae
Giovane Pianco <giovanepianco@MacBook-Pro.local>
2020-07-21 18:29:22 -0300

    Rotas assincronas

--- ProAgil.WebAPI/Controllers/ValuesController.cs
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WebAPI.Data;
 using ProAgil.WebAPI.Model;
 
 namespace ProAgil.WebAPI.Controllers
@@ -11,35 +14,48 @@ namespace ProAgil.WebAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        public DataContext _context { get; }
+
+        public ValuesController(DataContext context )
+        {
+            _context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<Evento>> Get()
+        public async Task<IActionResult> Get()
         {
-            return new Evento[]{
-                new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular e .NET Core",
-                    Local = "Ribeirão Preto",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    Lote = "1º Lote"
-                },
-                  new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular e outras coisas",
-                    Local = "São Paulo",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    Lote = "2º Lote"
-                },
-            };
+            try
+            {
+                var results = await _context.Eventos.ToListAsync(); 
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            } 
+            
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> Get(int id)
         {
-            return "value";
+            try
+            {
+                var results = await _context.Eventos.FirstOrDefaultAsync(
+                    e => e.EventoId == id
+                );
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                
+                return this.StatusCode(
+                    StatusCodes.Status500InternalServerError, "Banco de Dados Falhou"
+                );
+            }
+            
         }
 
         // POST api/values
