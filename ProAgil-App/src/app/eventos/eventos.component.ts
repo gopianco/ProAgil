78a2cb297ef3381a55e3f24c932f4a21ee6e7c4c
GIOVANE PIANCO <gopianco@hotmail.com>
2020-07-22 11:41:38 -0300

    adicionado boostrap e fa

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-eventos',
@@ -7,27 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any = [
-    {
-      EventoId: 1,
-      Tema: 'Angular',
-      Local: 'Ribeirão Preto',
-    },
-    {
-      EventoId: 2,
-      Tema: '.Net Core',
-      Local: 'São Paulo',
-    },
-    {
-      EventoId: 3,
-      Tema: '.Net Core e Angular',
-      Local: 'Ribeirão Preto',
-    },
-  ];
+  eventos: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getEventos();
+  }
+
+  getEventos(){
+    this.http.get('http://localhost:5000/api/values').subscribe(
+      response => {
+        this.eventos = response;
+      },
+      error =>{
+        console.log(error);
+      },
+    );
   }
 
 }
